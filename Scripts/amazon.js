595e38287ad06f850e2c03dc9ddbebf004456f29
Hardik <prajapatihardik375@gmail.com>
2025-11-02 19:11:48 +0530

    added all products

--- Scripts/amazon.js
+++ Scripts/amazon.js
@@ -1,35 +1,39 @@
-const products = [
-{
-    image : 'images/products/athletic-cotton-socks-6-pairs.jpg',
-    name : 'Black and Gray Athletic Cotton Socks - 6 Pairs',
-    ratigs : {
-        stars : 4.5,
-        count : 87
-            },
-    priceCents : 1090
-},
+// We have the data of all product in seperate file so we just added into the amazon.html page that helps to give that data.
 
-{
-    image : 'images/products/intermediate-composite-basketball.jpg',
-    name : 'Intermediate Size Basketball',
-    ratigs : {
-        stars : 4.0,
-        count : 127
-            },
-    priceCents : 2095
-},
+// const products = [
+// {
+//     image : 'images/products/athletic-cotton-socks-6-pairs.jpg',
+//     name : 'Black and Gray Athletic Cotton Socks - 6 Pairs',
+//     rating : {
+//         stars : 4.5,
+//         count : 87
+//             },
+//     priceCents : 1090
+// },
 
-{
-    image : 'images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg',
-    name : 'Adults Plain Cotton T-Shirt - 2 Pack',
-    ratigs : {
-        stars : 4.5,
-        count : 56
-            },
-    priceCents : 799
-}
+// {
+//     image : 'images/products/intermediate-composite-basketball.jpg',
+//     name : 'Intermediate Size Basketball',
+//     rating : {
+//         stars : 4.0,
+//         count : 127
+//             },
+//     priceCents : 2095
+// },
+
+// {
+//     image : 'images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg',
+//     name : 'Adults Plain Cotton T-Shirt - 2 Pack',
+//     rating : {
+//         stars : 4.5,
+//         count : 56
+//             },
+//     priceCents : 799
+// }
     
-];
+// ];
+
+
 
 // toFixed(2) gives two decimal numbers after point.
 
@@ -53,9 +57,9 @@ products.forEach((product)=>{
 
           <div class="product-rating-container">
             <img class="product-rating-stars"
-              src="images/ratings/rating-${product.ratigs.stars * 10 }.png">
+              src="images/ratings/rating-${product.rating.stars * 10 }.png">
             <div class="product-rating-count link-primary">
-              ${product.ratigs.count}
+              ${product.rating.count}
             </div>
           </div>
 
