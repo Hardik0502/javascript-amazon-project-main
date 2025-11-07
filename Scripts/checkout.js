65cd78a9d9537cb46f6cfda6cfc3d8f8130c2b52
Hardik <prajapatihardik375@gmail.com>
2025-11-07 12:58:57 +0530

    ..

--- Scripts/checkout.js
+++ Scripts/checkout.js
@@ -10,6 +10,8 @@ import dayjs from 'https://unpkg.com/supersimpledev@8.5.0/dayjs/esm/index.js'
 import { cart, removeproduct, updateDeliveryOption } from "../data/cart.js";
 import { getproduct, products } from "../data/products.js";
 import { deliveryopt, deliveryOptions } from '../data/deliverytiming.js';
+import { payment } from '../data/Payment.js';
+
 
 
 
@@ -150,73 +152,6 @@ import { deliveryopt, deliveryOptions } from '../data/deliverytiming.js';
 //         })
 // })
 
-function payment(){
 
-
-  let productPrice = 0;
-  let shippingPrice = 0;
-  let buyitem = 0;
-
-  cart.forEach((cartItem)=>{
-    
-    // For product with it's quantity , we made function in product.
-    let product = getproduct(cartItem.productId)
-    productPrice += product.priceCents * cartItem.quantity ;
-
-    let shippig = deliveryopt(cartItem.deliveryOptionId);
-    shippingPrice += shippig.priceCents ;
-
-    let totalItem = cartItem.quantity;
-    buyitem += totalItem;
-
-    const totalBeforeTax = productPrice + shippingPrice ;
-
-    const tax = totalBeforeTax * 0.1 ;
-
-    const total = totalBeforeTax + tax ;
-
-    let paymentHtml = ` 
-          <div class="payment-summary-title paymentsection">
-            Order Summary
-          </div>
-
-          <div class="payment-summary-row">
-            <div>Items (${buyitem}):</div>
-            <div class="payment-summary-money">$${(productPrice / 100).toFixed(2)}</div>
-          </div>
-
-          <div class="payment-summary-row">
-            <div>Shipping &amp; handling:</div>
-            <div class="payment-summary-money">$${(shippingPrice / 100).toFixed(2)}</div>
-          </div>
-
-          <div class="payment-summary-row subtotal-row">
-            <div>Total before tax:</div>
-            <div class="payment-summary-money">$${(totalBeforeTax / 100).toFixed(2)}</div>
-          </div>
-
-          <div class="payment-summary-row">
-            <div>Estimated tax (10%):</div>
-            <div class="payment-summary-money">$${(tax / 100).toFixed(2)}</div>
-          </div>
-
-          <div class="payment-summary-row total-row">
-            <div>Order total:</div>
-            <div class="payment-summary-money">$${(total / 100).toFixed(2)}</div>
-          </div>
-
-          <button class="place-order-button button-primary">
-            Place your order
-          </button>
-          
-          `;
-
-          document.querySelector('.payment-summary').innerHTML = paymentHtml;
-    
-    
-    })
-
-
-}
 
 payment();
